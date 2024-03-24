b08ab95317fb658e4272a72443a9402755ca37a3
gamer99122 <gamer99122@hotmail.com.tw>
2024-03-25 00:27:44 +0800

    Add Project

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>第一種TextInput判斷 顯示即時顯示輸入的號碼</Text>
+      <Text>第二種 TextInput判斷 顯示是否是正確判斷</Text>
       <StatusBar style="auto" />
     </View>
   );
